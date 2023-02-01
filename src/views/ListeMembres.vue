e4b05c8afc9e12c3edc7714d92754224d9966e46
WZidane <wail.zidane@hotmail.com>
2023-02-01 15:47:23 +0100

    Update.

--- src/views/ListeMembres.vue
+++ src/views/ListeMembres.vue
@@ -12,12 +12,12 @@ const membresStore = useMembresStore();
   <main>
     <h2 class="title">Liste des membres</h2>
     <ul>
-      <router-link to="/profil-membre" class="">
         <li class="box" v-for="membre in membresStore.state.membres">
-            <h2 class="title is-3">{{ membre.fullname }}</h2>
-            <p class="subtitle">{{ membre.email }}</p>
+            <router-link to="/profil-membre" class="">
+              <h2 class="title is-3">{{ membre.fullname }}</h2>
+              <p class="subtitle">{{ membre.email }}</p>
+            </router-link>
         </li>
-      </router-link>
     </ul>
   </main>
 </template>
