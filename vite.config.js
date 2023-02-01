4deb8358dd64704ed19fdb07bfd270fd5f46cd9f
WZidane <wail.zidane@hotmail.com>
2023-02-01 14:27:03 +0100

    Update.

--- vite.config.js
+++ vite.config.js
@@ -8,16 +8,16 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [
     vue(),
-/*    AutoImport({ 
-      imports: ['vue'],     
-      dirs: [
-        './src/stores',
-      ],       
-     })*/
+    /*    AutoImport({ 
+          imports: ['vue'],     
+          dirs: [
+            './src/stores',
+          ],       
+         })*/
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./', import.meta.url))
     }
   }
 })
